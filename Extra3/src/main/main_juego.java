b58f34881fece78b0f63eab9a0a55b3759280d99
Elias Barreto <eliasgobarreto@gmail.com>
2023-04-17 20:11:38 -0300

    cuarto commit

---
+++ Extra3/src/main/main_juego.java
@@ -0,0 +1,18 @@
+package main;
+
+import entidad.Juego;
+
+/**
+ *
+ * @author elias
+ */
+public class main_juego {
+
+    public static void main(String[] args) {
+        Juego j1 = new Juego();
+
+        j1.iniciar_juego();
+
+    }
+
+}
